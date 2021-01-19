d069ce2b4d38cd9f6ac56404b68f7f61f3edc440
Nachoo19 <70527239+Nachoo19@users.noreply.github.com>
2021-01-19 19:50:27 -0300

    Add files via upload

---
+++ CRUD AJAX/modelo/main.js
@@ -0,0 +1,6 @@
+const d = document,
+$table = d.querySelector(".crud_table"),
+$form = d.querySelector(".crud_form"),
+$title = d.querySelector(".crud_title"),
+$template = d.getElementById("crud_template").content,
+$fragment = d.createDocumentFragment();
